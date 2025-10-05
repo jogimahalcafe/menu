8981ba81f0e4da4fb51d48051cf1fc342cb89a2b
jogimahalcafe <arvind.cullen@gmail.com>
2025-10-05 08:45:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const carousel = document.getElementById('carousel');
-const indicatorsContainer = document.getElementById('indicators');
-const slides = document.querySelectorAll('.carousel-slide');
 
-slides.forEach((_, i) => {
-  const dot = document.createElement('span');
-  if (i === 0) dot.classList.add('active');
-  indicatorsContainer.appendChild(dot);
-});
-
-const dots = indicatorsContainer.querySelectorAll('span');
-
-carousel.addEventListener('scroll', () => {
-  const index = Math.round(carousel.scrollLeft / carousel.clientWidth);
-  dots.forEach(dot => dot.classList.remove('active'));
-  if (dots[index]) dots[index].classList.add('active');
-});
